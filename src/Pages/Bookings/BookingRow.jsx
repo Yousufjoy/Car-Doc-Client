7dca32abefa7dfab2ae4de93f5e06c360275adce
Yousufjoy <kanekiken333@gmail.com>
2023-11-06 13:26:23 +0600

    update booking using Patch

--- src/Pages/Bookings/BookingRow.jsx
+++ src/Pages/Bookings/BookingRow.jsx
@@ -1,11 +1,32 @@
-const BookingRow = ({ booking }) => {
-  const { customerName, email, date, service, price, img } = booking;
+const BookingRow = ({ booking, handleDelete, handleBookingConfirm }) => {
+  const { status, _id, customerName, email, date, service, price, img } =
+    booking;
 
   return (
     <tr>
       <th>
         <label>
-          <input type="checkbox" className="checkbox" />
+          <button
+            onClick={() => {
+              handleDelete(_id);
+            }}
+            className="btn btn-circle btn-outline"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
         </label>
       </th>
       <td>
@@ -19,7 +40,18 @@ const BookingRow = ({ booking }) => {
       <td>{date}</td>
       <td>${price}</td>
       <th>
-        <button className="btn btn-ghost btn-xs">details</button>
+        {status === "confirm" ? (
+          <span className=" font-bold text-primary">Confirmed!</span>
+        ) : (
+          <button
+            onClick={() => {
+              handleBookingConfirm(_id);
+            }}
+            className="btn btn-ghost btn-xs"
+          >
+            Please Confirm
+          </button>
+        )}
       </th>
     </tr>
   );
--- src/Pages/Bookings/Bookings.jsx
+++ src/Pages/Bookings/Bookings.jsx
@@ -14,14 +14,51 @@ const Bookings = () => {
       .then((data) => {
         setBookings(data);
       });
-  }, []);
+  }, [url]);
 
+  const handleDelete = (id) => {
+    const proceed = confirm("Are you sure you want to delet?");
+    if (proceed) {
+      fetch(`http://localhost:5000/bookings/${id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data.deletedCount > 0) {
+            alert("Succesfully deleted!");
+            const remaining = bookings.filter((booking) => booking._id !== id);
+            setBookings(remaining);
+          }
+        });
+    }
+  };
+
+  const handleBookingConfirm = (id) => {
+    fetch(`http://localhost:5000/bookings/${id}`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ status: "confirm" }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount > 0) {
+          const remaining = bookings.filter((booking) => booking._id !== id);
+          const updated = bookings.find((booking) => booking._id == id);
+          updated.status = "confirm";
+          const newBookings = [updated, ...remaining];
+          setBookings(newBookings);
+        }
+      });
+  };
   return (
     <div>
       <h2 className="text-5xl"> Your Bookings : {bookings.length}</h2>
       <div className="overflow-x-auto">
         <table className="table">
-          {/* head */}
           <thead>
             <tr>
               <th>
@@ -29,8 +66,8 @@ const Bookings = () => {
                   <input type="checkbox" className="checkbox" />
                 </label>
               </th>
-              <th>Name</th>
-              <th></th>
+              <th>Image</th>
+              <th>Service</th>
               <th>Price</th>
               <th>Email</th>
               <th>Date</th>
@@ -38,7 +75,12 @@ const Bookings = () => {
           </thead>
           <tbody>
             {bookings.map((booking) => (
-              <BookingRow key={booking._id} booking={booking}></BookingRow>
+              <BookingRow
+                key={booking._id}
+                handleDelete={handleDelete}
+                handleBookingConfirm={handleBookingConfirm}
+                booking={booking}
+              ></BookingRow>
             ))}
           </tbody>
         </table>
